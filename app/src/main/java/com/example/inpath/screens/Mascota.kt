48e81ab4a9fca5a3c27a05f780f86db1e889b97c
Gabriel <gb9@alumnes.udl.cat>
2025-03-24 13:34:36 +0100

    mascota ahora es saveable

--- app/src/main/java/com/example/inpath/screens/Mascota.kt
+++ app/src/main/java/com/example/inpath/screens/Mascota.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,7 +27,7 @@ import com.example.inpath.R
 @Composable
 fun Mascota(navController: NavController, viewModel: MascotaViewModel = viewModel()) {
     var mostrarMenu by remember { mutableStateOf(false) }
-    var mascotaSeleccionada by remember { mutableStateOf("") }
+    var mascotaSeleccionada by rememberSaveable { mutableStateOf(viewModel.mascotaSeleccionada) }
 
     Column(
         modifier = Modifier
